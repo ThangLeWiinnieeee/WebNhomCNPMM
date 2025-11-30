db94b949bfdbb155f08590878601d82987a5a539
NT-THTRM\nthuy <nthuytram2815@gmail.com>
2025-11-30 11:44:27 +0700

    push order

--- wedding-project-be/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getUserProfile, updateUserProfile } = require("../controllers/userController");
-
-router.get("/:id", getUserProfile);
-router.put("/:id", updateUserProfile);
-
-module.exports = router;
--- wedding-project-fe/src/testData.js
+++
@@ -1,8 +0,0 @@
-export const testProfile = {
-  _id: "644f0b2c1234567890abcdef",
-  name: "Nguyen Van A",
-  email: "test@example.com",
-  phone: "0123456789",
-  avatar: "",
-  bio: "Bio test user"
-};
